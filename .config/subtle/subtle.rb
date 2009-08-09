2653cc4d59cac72b940428b490e6f0d4aaaa3950
Christoph Kappel <christoph@unexist.dev>
2009-08-09 22:04:21 +0200

    Updated config

--- .config/subtle/subtle.rb
+++ .config/subtle/subtle.rb
@@ -13,6 +13,7 @@ OPTIONS = {
   :step    => 5,                                   # Window move/resize key step
   :snap    => 10,                                  # Window border snapping
   :gravity => 5,                                   # Default gravity
+  :urgent  => true,                                # Make transient windows urgent
   :padding => [ 0, 0, 0, 0 ],                      # Screen padding (left, right, top, bottom)
   :font    => "-*-snap-*-*-*-*-10-*-*-*-*-*-*-*"   # Font string
 }
@@ -40,7 +41,7 @@ COLORS = {
   :fg_focus      => "#000000",  # Foreground color of focus window title and view
 
   :bg_panel      => "#444444",  # Background color of panel
-  :bg_views      => "#3d3d3d",  # Background color of view button
+  :bg_views      => "#444444",  # Background color of view button
   :bg_sublets    => "#6699CC",  # Background color of sublets
   :bg_focus      => "#6699CC",  # Background color of focus window title and view
 
@@ -74,53 +75,53 @@ COLORS = {
 #
 
 GRABS = {
-  "W-1"      => :ViewJump1,                 # Jump to view 1
-  "W-2"      => :ViewJump2,                 # Jump to view 2
-  "W-3"      => :ViewJump3,                 # Jump to view 3
-  "W-4"      => :ViewJump4,                 # Jump to view 4
+  "W-1"       => :ViewJump1,                 # Jump to view 1
+  "W-2"       => :ViewJump2,                 # Jump to view 2
+  "W-3"       => :ViewJump3,                 # Jump to view 3
+  "W-4"       => :ViewJump4,                 # Jump to view 4
 
-  "W-S-1"    => :ScreenJump1,               # Jump to view 1
-  "W-S-2"    => :ScreenJump2,               # Jump to view 2
-  "W-S-3"    => :ScreenJump3,               # Jump to view 3
-  "W-S-4"    => :ScreenJump4,               # Jump to view 4
+  "W-S-1"     => :ScreenJump1,               # Jump to view 1
+  "W-S-2"     => :ScreenJump2,               # Jump to view 2
+  "W-S-3"     => :ScreenJump3,               # Jump to view 3
+  "W-S-4"     => :ScreenJump4,               # Jump to view 4
 
-  "W-C-q"    => :SubtleQuit,                # Quit subtle
-  "W-C-r"    => :SubtleReload,              # Reload subtle
+  "W-C-q"     => :SubtleQuit,                # Quit subtle
+  "W-C-r"     => :SubtleReload,              # Reload subtle
 
-  "W-B1"     => :WindowMove,                # Move window
-  "W-B3"     => :WindowResize,              # Resize window
-  "W-f"      => :WindowFloat,               # Toggle float
-  "W-space"  => :WindowFull,                # Toggle full
-  "W-s"      => :WindowStick,               # Toggle stick
-  "W-r"      => :WindowRaise,               # Raise window
-  "W-l"      => :WindowLower,               # Lower window
-  "W-Left"   => :WindowLeft,                # Select window left
-  "W-Down"   => :WindowDown,                # Select window below
-  "W-Up"     => :WindowUp,                  # Select window above
-  "W-Right"  => :WindowRight,               # Select window right
-  "W-k"      => :WindowKill,                # Kill window
+  "W-B1"      => :WindowMove,                # Move window
+  "W-B3"      => :WindowResize,              # Resize window
+  "W-S-f"     => :WindowFloat,               # Toggle float
+  "W-S-space" => :WindowFull,                # Toggle full
+  "W-S-s"     => :WindowStick,               # Toggle stick
+  "W-r"       => :WindowRaise,               # Raise window
+  "W-l"       => :WindowLower,               # Lower window
+  "W-Left"    => :WindowLeft,                # Select window left
+  "W-Down"    => :WindowDown,                # Select window below
+  "W-Up"      => :WindowUp,                  # Select window above
+  "W-Right"   => :WindowRight,               # Select window right
+  "W-k"       => :WindowKill,                # Kill window
 
-  "A-S-1"    => :WindowScreen1,             # Set screen 1
-  "A-S-2"    => :WindowScreen2,             # Set screen 2
-  "A-S-3"    => :WindowScreen3,             # Set screen 3
-  "A-S-4"    => :WindowScreen4,             # Set screen 4
+  "A-S-1"     => :WindowScreen1,             # Set screen 1
+  "A-S-2"     => :WindowScreen2,             # Set screen 2
+  "A-S-3"     => :WindowScreen3,             # Set screen 3
+  "A-S-4"     => :WindowScreen4,             # Set screen 4
 
-  "W-KP_7"   => :GravityTopLeft,            # Set top left gravity
-  "W-KP_8"   => :GravityTop,                # Set top gravity
-  "W-KP_9"   => :GravityTopRight,           # Set top right gravity
-  "W-KP_4"   => :GravityLeft,               # Set left gravity
-  "W-KP_5"   => :GravityCenter,             # Set center gravity
-  "W-KP_6"   => :GravityRight,              # Set right gravity
-  "W-KP_1"   => :GravityBottomLeft,         # Set bottom left gravity
-  "W-KP_2"   => :GravityBottom,             # Set bottom gravity
-  "W-KP_3"   => :GravityBottomRight,        # Set bottom right gravity
+  "W-q"       => :GravityTopLeft,            # Set top left gravity
+  "W-w"       => :GravityTop,                # Set top gravity
+  "W-e"       => :GravityTopRight,           # Set top right gravity
+  "W-a"       => :GravityLeft,               # Set left gravity
+  "W-s"       => :GravityCenter,             # Set center gravity
+  "W-d"       => :GravityRight,              # Set right gravity
+  "W-y"       => :GravityBottomLeft,         # Set bottom left gravity
+  "W-x"       => :GravityBottom,             # Set bottom gravity
+  "W-c"       => :GravityBottomRight,        # Set bottom right gravity
 
-  "W-u"      => "uzbl",                     # Exec uzbl
-  "W-Return" => "urxvt",                    # Exec a term
-  "W-x"      => @dmenu,                     # Exec dmenu
+  "W-u"       => "uzbl",                     # Exec uzbl
+  "W-Return"  => "urxvt",                    # Exec a term
+  "W-m"       => @dmenu,                     # Exec dmenu
 
-  "S-F2"     => lambda { |c| puts c.name }, # Print client name
-  "S-F3"     => lambda {  puts version   }  # Print subtlext version
+  "S-F2"      => lambda { |c| puts c.name }, # Print client name
+  "S-F3"      => lambda {  puts version   }  # Print subtlext version
 }
 
 # Tags
@@ -129,21 +130,23 @@ TAGS = {
   "terms"   => "urxvt|xterm",
   "test"    => "xephyr",
   "gimp"    => "gimp",
-  "browser" => { :regex => "swiftweasel|uzbl", :screen => 2, :gravity => 5 },
-  "editor"  => { :regex => "[g]?vim", :gravity => 8 },
-  "stick"   => { :regex => "mplayer|evince|display|chrom", :stick => true, :float => true, :screen => 2 },
-  "float"   => { :regex => "xephyr|gimp", :float => true, :screen => 2 },
-  "top"     => { :regex => "urxvt1|[g]?vim", :gravity => 8 },
+  "skype"   => "skype",
+  "browser" => { :regex => "swiftweasel|uzbl", :gravity => 5 },
+  "editor"  => { :regex => "[g]?vim", :gravity => 5 },
+  "stick"   => { :regex => "mplayer|apvlv|display|chrom", :stick => true, :float => true },
+  "float"   => { :regex => "xephyr|gimp", :float => true },
+  "top"     => { :regex => "urxvt1", :gravity => 8 },
   "bottom"  => { :regex => "urxvt2", :gravity => 2 }
 }  
 
 #
 # Views
 #
-VIEWS = {
-  "web"  => "gimp|browser|editor|terms",
-  "dev"  => "test|editor|terms"
-}
+VIEWS = [
+  { "terms" => "terms" },
+  { "www"   => "default|skype|gimp|browser" },
+  { "dev"   => "editor" }
+]
 
 #
 # Hooks
