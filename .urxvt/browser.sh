fd8bb34e4ba71e383cc203cf70e6cb4c07d1aed6
Christoph Kappel <christoph@unexist.dev>
2024-03-15 18:03:08 +0100

    Updated configs

--- .urxvt/browser.sh
+++ .urxvt/browser.sh
@@ -4,4 +4,6 @@ if [ "`pidof firefox`" ]; then
   firefox -P default -new-tab $1
 elif [ "`pidof chromium`" ]; then
   chromium -new-tab $1
+elif [ "`pidof brave-browser`" ]; then
+  brave-browser -new-tab $1
 fi
