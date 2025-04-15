7c865843a58e53e14e62c2d8b251746d860e92e7
Christoph Kappel <christoph@unexist.dev>
2025-04-15 08:48:10 +0200

    Updated for new screen

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -2,6 +2,11 @@
 
 if [ "xsingle" = "x$1" ] ; then
     xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --dpi 96
+elif [ "xcurved" = "x$1" ] ; then
+    xrandr \
+        --output DP-3 --mode 5120x1440 --pos 0x0 --primary \
+        --output eDP-1 --mode 1920x1200 --pos 1920x0 --right-of DP-3 \
+        --dpi 96
 elif [ "xwork" = "x$1" ] ; then
     xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 \
         --output DP-1 --mode 1920x1080 --pos 3840x0 --right-of eDP-1 --primary \
@@ -13,4 +18,4 @@ else
         --dpi 96
 fi
 
-subtler -R
+#subtler -R
