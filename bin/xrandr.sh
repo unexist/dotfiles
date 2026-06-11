5db90f7ff249775b57890c83b2c2a41cea021147
Christoph Kappel <christoph@unexist.dev>
2026-06-11 13:27:42 +0200

    Added right mode

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -7,7 +7,11 @@ elif [ "xcurved" = "x$1" ] ; then
         --output DP-1 --mode 5120x1440 --pos 0x0 --primary \
         --output eDP-1 --mode 1920x1200 --pos 1920x0 --right-of DP-1 \
         --dpi 96
-    #xrandr --output DP-1 --brightness 0.6
+elif [ "xright" = "x$1" ] ; then
+    xrandr \
+        --output DP-3 --mode 5120x1440 --pos 0x0 --primary \
+        --output eDP-1 --mode 1920x1200 --pos 1920x0 --left-of DP-3 \
+        --dpi 96
 elif [ "xwork" = "x$1" ] ; then
     xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 \
         --output DP-4 --mode 1920x1080 --pos 3840x0 --right-of eDP-1 --primary \
